eee9709ccfa9f544a345a617251fbc35b004be33
tanya-kostecki <tanya.pavlova1294@gmail.com>
2023-12-12 10:52:15 +0100

    sign up and register pages

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import * as S from './header.styles'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <S.Header>
-        <S.HeaderNav>
-            <S.HeaderButton className='header__button'>Вход в личный кабинет</S.HeaderButton>
-        </S.HeaderNav>
+      <S.HeaderNav>
+        <NavLink to='/login' style={{'textDecoration': 'none'}}>
+          <S.HeaderButton className="header__button">
+            Вход в личный кабинет
+          </S.HeaderButton>
+        </NavLink>
+      </S.HeaderNav>
     </S.Header>
-  )
+  );
 }
 
 export default Header
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MainPage from './pages/main/MainPage'
 import ProfilePage from './pages/profile/ProfilePage'
+import AuthPage from './pages/authorization/AuthPage'
 import { Routes, Route } from 'react-router-dom'
 
 const AppRoutes = () => {
@@ -8,6 +9,8 @@ const AppRoutes = () => {
     <Routes>
         <Route path='/' element={<MainPage/>}></Route>
         <Route path='/profile' element={<ProfilePage/>}></Route>
+        <Route path='/login' element={<AuthPage isLoginMode={true}/>}></Route>
+        <Route path='/signup' element={<AuthPage isLoginMode={false}/>}></Route>
     </Routes>
   )
 }
